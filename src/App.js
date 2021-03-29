52da3104bac456a29f3bcd9bcb0b39febd141a2a
Sandro-Col <sandro.col@gmail.com>
2021-03-29 15:52:09 -0300

    integrated firebase into application, create sign in page with sign in component and sign in with google

--- src/App.js
+++ src/App.js
@@ -4,17 +4,46 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
+import Header from './components/header/header.component.jsx';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div>
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-      </Switch>
-      
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unSubscribeFormAuth = null;
+
+  componentDidMount() {
+    this.unSubscribeFormAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+
+      console.log(user)
+    });
+  }
+
+  componentWillUnmount() {
+    this.unSubscribeFormAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={ this.state.currentUser } />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
