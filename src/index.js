948385204f0a4e853501273b9480e0184514183e
Sandro-Col <sandro.col@gmail.com>
2021-04-04 18:49:42 -0300

    updating our application with reselect, replaced cart selector with memoized selectors

--- src/index.js
+++ src/index.js
@@ -14,9 +14,7 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-    <React.StrictMode>
       <App />
-    </React.StrictMode>
     </BrowserRouter>
   </Provider>  
   ,
